4b59c6277ffb266847949431dfbf3425f40b7ec7
Josh Harvey <josh@lamassu.is>
2014-09-11 12:16:00 -0400

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-var raw = require("./build/Release/v4l2camera");
+'use strict';
 
-exports.Camera = function Camera() {
-    var args = [null].concat([].slice.call(arguments));
-    var ctor = raw.Camera.bind.apply(raw.Camera, args);
-    return new ctor;
-};
+var fs = require('fs');
+var async = require('async');
+
+var cam = require('./build/Release/seret');
+
+var fd = cam.cameraOn('/dev/video0', 960, 720);
+var buffer = new Buffer(960 * 720);
+
+var t0 = Date.now();
+setInterval(function() { var elapsed = (Date.now() - t0) / 1000; console.log('ping: %d', elapsed); }, 1000);
+cam.startCapture(fd);
+
+function captureFrame() {
+  cam.captureFrame(fd, buffer);
+}
+
+async.timesSeries(100, function(i, next) {
+  if (i % 10 === 0) console.log(i);
+  captureFrame();
+  fs.writeFileSync('./result.gray', buffer);
+  setTimeout(next, 10);
+}, function() {
+  cam.stopCapture(fd);
+  cam.cameraOff(fd);
+});
+
--- src/camera.h
+++ src/camera.h
@@ -1,5 +1,7 @@
-int camera_on(char *dev_name, uint32_t width, uint32_t height);
-void camera_off(int fd);
-void start_capturing(int fd);
-void stop_capturing(int fd);
-void capture_frame(int fd, char *result_buf, size_t result_size);
+extern "C" {
+  int camera_on(char *dev_name, uint32_t width, uint32_t height);
+  void camera_off(int fd);
+  void start_capturing(int fd);
+  void stop_capturing(int fd);
+  void capture_frame(int fd, char *result_buf, size_t result_size);
+}
